2a29a5305dd8d3f5ab5324dd709bc4e45824ad93
codershona <falgunimst95@gmail.com>
2020-08-14 12:40:42 +0200

    wikipedia search engine api key added

--- src/App.js
+++ src/App.js
@@ -26,7 +26,31 @@ class App extends React.Component {
     });
 
     const pointerToThis = this;
-    console.log(this);
+    // console.log(this);
+
+    
+   
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "query",
+    list: "search",
+    srsearch: "Nelson Mandela",
+    format: "json"
+};
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {
+        if (response.query.search[0].title === "Nelson Mandela"){
+            console.log("Your search page 'Nelson Mandela' exists on English Wikipedia" );
+        }
+    })
+    .catch(function(error){console.log(error);});
+
 
   }
 
