f519f1b6a0423920588bf71dcbf79d8014bab3a3
codershona <falgunimst95@gmail.com>
2020-08-14 15:23:51 +0200

    got some results

--- src/App.js
+++ src/App.js
@@ -62,10 +62,21 @@ var params = {
       .then(
 
         function (response) {
-          console.log(response.query.search[0].pageid);
+         // console.log(response.query.search[0].pageid);
 
 
           for (var key in response.query.search) {
+
+            pointerToThis.state.wikiSearchReturnValues.push({
+
+              queryResultPageFullURL: 'no link',
+              queryResultPageID: response.query.search[key].pageid,
+          queryResultPageTitle: response.query.search[key].title,
+          queryResultPageSnippet: response.query.search[key].snippet
+
+
+
+             });
 
 
           }
@@ -77,8 +88,53 @@ var params = {
       )
 
 
+      .then(
+
+        function (response) {
+          for (var key2 in pointerToThis.state.wikiSearchReturnValues) {
+    
+    console.log(pointerToThis.state.wikiSearchReturnValues);
+
+  
+     let page = pointerToThis.state.wikiSearchReturnValues[key2];
+ 
+   let pageID = page.queryResultPageID;
+
+     let urlForRetrievingPageURLByPageID = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageID}&inprop=url&format=json`;
+
+  fetch(urlForRetrievingPageURLByPageID)
+    .then(
+
+      function (response) {
+
+        return response.json();
+
+      }
+
+  )
+
+    .then(
+
+      function (response) {
+        page.queryResultPageFullURL = response.query.pages[pageID].fullurl;
+
+      pointerToThis.forceUpdate();
 
 
+      }
+
+      )
+
+
+
+          }
+
+
+
+        }
+
+
+   )
 
 }
 
