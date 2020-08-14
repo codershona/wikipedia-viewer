6694b1ddc4a0bc91ff198e8aaba9d3ca396ef56f
codershona <falgunimst95@gmail.com>
2020-08-14 15:51:54 +0200

    trying to fix error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,219 @@
-import React from 'react';
+// import React from 'react';
 
+// import './index.css';
+
+// class App extends React.Component {
+
+//   constructor(props) {
+
+//     super(props);
+
+//     this.state = {
+
+//     wikiSearchReturnValues: [],
+//       wikiSearchTerms: ''
+//     }
+
+//   }
+
+
+//   useWikiSearchEngine = (e) => {
+
+//     e.preventDefault();
+
+//     this.setState({
+//       wikiSearchReturnValues: []
+//     });
+
+//     const pointerToThis = this;
+//     // console.log(this);
+
+    
+   
+// var url = "https://en.wikipedia.org/w/api.php"; 
+
+// var params = {
+//     action: "query",
+//     list: "search",
+//     srsearch: "this.state.WikiSearchTerms",
+//     format: "json"
+// };
+
+//     url = url + '?origin=*';
+//     Object.keys(params).forEach((key) => {
+//       url += "&" + key + "=" + params[key];
+
+//       });
+
+//     fetch(url)
+//       .then(
+
+//         function (response) {
+          
+//           return response.json();
+      
+
+//         }
+
+
+
+//     ) 
+
+//       .then(
+
+//         function (response) {
+//          // console.log(response.query.search[0].pageid);
+
+
+//           for (var key in response.query.search) {
+
+//             pointerToThis.state.wikiSearchReturnValues.push({
+
+//               queryResultPageFullURL: 'no link',
+//               queryResultPageID: response.query.search[key].pageid,
+//           queryResultPageTitle: response.query.search[key].title,
+//           queryResultPageSnippet: response.query.search[key].snippet
+
+
+
+//              });
+
+
+//           }
+
+
+//         }
+
+
+//       )
+
+
+//       .then(
+
+//         function (response) {
+//           for (var key2 in pointerToThis.state.wikiSearchReturnValues) {
+    
+//    // console.log(pointerToThis.state.wikiSearchReturnValues);
+
+  
+//      let page = pointerToThis.state.wikiSearchReturnValues[key2];
+ 
+//    let pageID = page.queryResultPageID;
+
+//      let urlForRetrievingPageURLByPageID = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageID}&inprop=url&format=json`;
+
+//   fetch(urlForRetrievingPageURLByPageID)
+//     .then(
+
+//       function (response) {
+
+//         return response.json();
+
+//       }
+
+//   )
+
+//     .then(
+
+//       function (response) {
+    
+//     page.queryResultPageFullURL = response.query.pages[pageID].fullurl;
+
+//       pointerToThis.forceUpdate();
+
+
+//       }
+
+//       )
+
+
+
+//           }
+
+
+
+//         }
+
+
+//    )
+
+// }
+
+
+
+
+
+
+//   changeWkikiSearchTerm = (e) => {
+//     this.setState({
+//       wikiSearchTerms: e.target.value
+//     });
+
+
+//   }
+   
+//    render() {
+
+//     let wikiSearchResults = [];
+
+//     console.log(this.state.wikiSearchReturnValues);
+
+//     for (var key3 in this.state.wikiSearchReturnValues) {
+
+
+//       wikiSearchResults.push(
+
+//          <div className="searchResultDiv" key={key3}>
+//           <h3><a href={this.state.wikiSearchReturnValues[key3].queryResultPageFullURL}>{this.state.wikiSearchReturnValues[key3].queryResultPageTitle}</a></h3>
+//           <span className='link'><a href={this.state.wikiSearchReturnValues[key3].queryResultPageFullURL}>{this.state.wikiSearchReturnValues[key3].queryResultPageFullURL}</a></span>
+//           <p className="description" dangerouslySetInnerHTML={{__html: this.state.wikiSearchReturnValues[key3].queryResultPageSnippet}}></p>
+//         </div>
+
+
+//       );
+
+
+
+//     }
+
+//      return (
+//      <div className="App">
+//         <h1>WIKIPEDIA VIEWER</h1>
+//         <form action="">
+//           <input type="text" value={this.state.WikiSearchTerms || ''} onChange={this.changeWikiSearchTerms} placeholder='Search Wikipedia Articles' />
+//           <button type='submit' onClick={this.useWikiSearchEngine}>Search</button>
+//         </form>
+//         {wikiSearchResults}
+//       </div>
+//   );
+
+
+
+//    }
+
+
+ 
+// }
+
+// export default App;
+
+
+
+
+
+import React from 'react';
 import './index.css';
 
 class App extends React.Component {
-
   constructor(props) {
-
     super(props);
-
     this.state = {
-
-    wikiSearchReturnValues: [],
+      wikiSearchReturnValues: [],
       wikiSearchTerms: ''
     }
-
   }
 
-
   useWikiSearchEngine = (e) => {
-
     e.preventDefault();
 
     this.setState({
@@ -26,173 +221,100 @@ class App extends React.Component {
     });
 
     const pointerToThis = this;
-    // console.log(this);
 
-    
-   
-var url = "https://en.wikipedia.org/w/api.php"; 
+    var url = "https://en.wikipedia.org/w/api.php";
 
-var params = {
-    action: "query",
-    list: "search",
-    srsearch: "this.state.WikiSearchTerms",
-    format: "json"
-};
+    var params = {
+      action: 'query',
+      list: 'search',
+      srsearch: this.state.WikiSearchTerms,
+      format: 'json'
+    };
 
     url = url + '?origin=*';
     Object.keys(params).forEach((key) => {
       url += "&" + key + "=" + params[key];
-
-      });
+    });
 
     fetch(url)
       .then(
-
         function (response) {
-          
           return response.json();
-      
-
         }
-
-
-
-    ) 
-
+      )
       .then(
-
         function (response) {
-         // console.log(response.query.search[0].pageid);
-
+          // console.log(response);
 
           for (var key in response.query.search) {
-
             pointerToThis.state.wikiSearchReturnValues.push({
-
               queryResultPageFullURL: 'no link',
               queryResultPageID: response.query.search[key].pageid,
-          queryResultPageTitle: response.query.search[key].title,
-          queryResultPageSnippet: response.query.search[key].snippet
-
-
-
-             });
-
-
+              queryResultPageTitle: response.query.search[key].title,
+              queryResultPageSnippet: response.query.search[key].snippet
+            });
           }
-
-
         }
-
-
       )
-
-
       .then(
-
         function (response) {
           for (var key2 in pointerToThis.state.wikiSearchReturnValues) {
-    
-   // console.log(pointerToThis.state.wikiSearchReturnValues);
+            // console.log(pointerToThis.state.wikiSearchReturnValues);
+            let page = pointerToThis.state.wikiSearchReturnValues[key2];
+            let pageID = page.queryResultPageID;
+            let urlForRetrievingPageURLByPageID = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageID}&inprop=url&format=json`;
 
-  
-     let page = pointerToThis.state.wikiSearchReturnValues[key2];
- 
-   let pageID = page.queryResultPageID;
+            fetch(urlForRetrievingPageURLByPageID)
+              .then(
+                function (response) {
+                  return response.json();
+                }
+              )
+              .then(
+                function (response) {
+                  page.queryResultPageFullURL = response.query.pages[pageID].fullurl;
 
-     let urlForRetrievingPageURLByPageID = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageID}&inprop=url&format=json`;
-
-  fetch(urlForRetrievingPageURLByPageID)
-    .then(
-
-      function (response) {
-
-        return response.json();
-
-      }
-
-  )
-
-    .then(
-
-      function (response) {
-    
-    page.queryResultPageFullURL = response.query.pages[pageID].fullurl;
-
-      pointerToThis.forceUpdate();
-
-
-      }
-
-      )
-
-
-
+                  pointerToThis.forceUpdate();
+                }
+              )
           }
-
-
-
         }
-
-
-   )
-
-}
-
-
-
-
-
-
-  changeWkikiSearchTerm = (e) => {
-    this.setState({
-      wikiSearchTerms: e.target.value
-    });
-
-
+      )
   }
-   
-   render() {
 
+  changeWikiSearchTerms = (e) => {
+    this.setState({
+      WikiSearchTerms: e.target.value
+    });
+  }
+
+  render() {
     let wikiSearchResults = [];
-
-    console.log(this.state.wikiSearchReturnValues);
+    // console.log(this.state.wikiSearchReturnValues);
 
     for (var key3 in this.state.wikiSearchReturnValues) {
-
-
       wikiSearchResults.push(
-
-         <div className="searchResultDiv" key={key3}>
+        <div className="searchResultDiv" key={key3}>
           <h3><a href={this.state.wikiSearchReturnValues[key3].queryResultPageFullURL}>{this.state.wikiSearchReturnValues[key3].queryResultPageTitle}</a></h3>
           <span className='link'><a href={this.state.wikiSearchReturnValues[key3].queryResultPageFullURL}>{this.state.wikiSearchReturnValues[key3].queryResultPageFullURL}</a></span>
           <p className="description" dangerouslySetInnerHTML={{__html: this.state.wikiSearchReturnValues[key3].queryResultPageSnippet}}></p>
         </div>
-
-
       );
-
-
-
     }
 
-     return (
-     <div className="App">
-        <h1>WIKIPEDIA VIEWER</h1>
+    console.log(wikiSearchResults);
+
+    return (
+      <div className="App">
+        <h1>Wikipedia Search Engine</h1>
         <form action="">
           <input type="text" value={this.state.WikiSearchTerms || ''} onChange={this.changeWikiSearchTerms} placeholder='Search Wikipedia Articles' />
           <button type='submit' onClick={this.useWikiSearchEngine}>Search</button>
         </form>
         {wikiSearchResults}
       </div>
-  );
-
-
-
-   }
-
-
- 
+    );
+  }
 }
 
 export default App;
